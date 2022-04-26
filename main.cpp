caa4b9954df3604fa0073fdcef0c7292bed6e1f5
Angelo Pahati <aesgu001@github.com>
2022-04-25 20:07:05 -0700

    Add display game board function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 
+/*
+*  Writes the current game board to standard output.
+*
+*  @param   gameBoard the game board to display.
+*  @param   boardSize the game board's size.
+*
+*  @return  None.
+*/
+void displayGameBoard(char gameBoard[], int boardSize)
+{
+    for (int i = 0; i < boardSize; ++i)
+    {
+        std::cout << " " << gameBoard[i] << " |";
+        if ((i + 1) % 3 == 0)
+        {
+            std::cout << "\n------------\n";
+        }
+    }
+    std::cout << "\n";
+}
+
 int main()
 {
     char gameBoard[9]
@@ -12,15 +33,7 @@ int main()
     char playerOne = 'X';
     char playerTwo = 'O';
 
-    for (int i = 0; i < 9; ++i)
-    {
-        std::cout << " " << gameBoard[i] << " |";
-        if ((i + 1) % 3 == 0)
-        {
-            std::cout << "\n------------\n";
-        }
-    }
-    std::cout << "\n";
+    displayGameBoard(gameBoard, 9);
 
     std::cout << "Player One: " << playerOne << ", Player Two: " << playerTwo << "\n";
     return 0;
